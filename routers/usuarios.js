9031e4e98552c0092c16d85c4c0c0f9667cefba8
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-07-23 21:33:52 -0500

    feat(EndPoint): :sparkles: Creacion EndPoint filter por nacionalidad
    se creo un endPoint que nos filtra todos los usuarios de una misma nacionalidad
    
    by:Carlos Villafrades

--- routers/usuarios.js
+++ routers/usuarios.js
@@ -89,5 +89,21 @@ storageUsuario.put("/",generateToken, validateTokenEndpoints, validateUsuario, (
     )
 })
 
+//ENDPOINTS
+
+//endPoint que me filtre los usuarios por nacionalidad
+storageUsuario.get("/nacionalidad/:idNacionalidad", (req,res)=>{ 
+    con.query(
+        `SELECT usu_nombre AS "nombre_completo", usu_apodo AS "apodo", usu_genero_fk AS "genero_id", gen_nombre AS "genero", usu_edad AS "edad", usu_descripcion AS "descripcion", usu_image AS "image", usu_ciudad_fk AS "ciudad_id", ciu_nombre AS "ciudad", pai_id AS "id_pais", pai_nombre AS "pais" FROM Usuario INNER JOIN Genero ON usu_genero_fk = gen_id INNER JOIN Ciudad ON usu_ciudad_fk = ciu_id INNER JOIN Region ON ciu_region_fk = reg_id INNER JOIN Pais ON reg_pais_fk = pai_id WHERE pai_id = ?`, req.params.idNacionalidad,
+        (err,data,fil)=>{
+            if (err) {
+               res.status(401).send("Error en la solicitud") 
+            }else{
+                res.send(data)
+            }
+        }
+    )
+})
+
 
 export default storageUsuario;
